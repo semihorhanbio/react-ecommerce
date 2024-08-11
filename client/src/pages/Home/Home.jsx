363409195643420ae01e1c0f07bb6498fded7481
Semih Orhan <sorhan3.14@gmail.com>
2024-08-11 16:19:53 +0300

    finish categories component

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,15 @@
 import Slider from '../../components/Slider/Slider'
 import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts'
+import Categories from '../../components/FeaturedProducts/Categories'
+
 import "./Home.scss"
 
 const Home = () => {
   return (
     <div className='home'>
       <Slider/>
-      <FeaturedProducts type="featured"/>      
+      <FeaturedProducts type="featured"/> 
+      <Categories/>     
       <FeaturedProducts type="trending"/>
     </div>
   )
