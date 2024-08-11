53fc2dfc77e9f8e5b9d56cce421b492d58d79e78
Semih Orhan <sorhan3.14@gmail.com>
2024-08-11 15:36:12 +0300

    finish slider on home page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,6 +1,11 @@
+import Slider from '../../components/Slider/Slider'
+import "./Home.scss"
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <Slider/>
+    </div>
   )
 }
 
