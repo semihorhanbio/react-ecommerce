a78223e89867c2fc425d6cdb2a7f87ae3afeb573
Semih Orhan <sorhan3.14@gmail.com>
2024-08-11 16:16:35 +0300

    finish feeatured product design

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import Slider from '../../components/Slider/Slider'
+import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts'
 import "./Home.scss"
 
 const Home = () => {
   return (
     <div className='home'>
       <Slider/>
+      <FeaturedProducts type="featured"/>      
+      <FeaturedProducts type="trending"/>
     </div>
   )
 }
